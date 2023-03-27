5fe5913d710b18abfb9ff35963b7ee4554c48652
a-sad-dan <danishasad91@gmail.com>
2023-03-28 02:50:13 +0530

    Initial setup of the page

---
+++ script.js
@@ -0,0 +1,24 @@
+// Selecting the input and output elements
+const sliderInput = document.querySelector('#count');
+const sliderOutput = document.querySelector("#grid_size");
+// Setting the Default value of the slider
+sliderInput.value = 16;
+// Output the default value
+sliderOutput.textContent = sliderInput.value;
+// output Value of on every change
+sliderInput.addEventListener("input", () => {
+    sliderOutput.textContent = sliderInput.value;
+});
+
+
+// Making divs for the pad
+
+const pad = document.querySelector("#pad");
+
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        newDiv = document.createElement("div");
+        pad.appendChild(newDiv);
+    }
+}
